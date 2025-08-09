baad180350770146486acc47cb1eac7603723eb0
Kakada04 <kakada04as@gmail.com>
2025-08-09 21:33:46 +0700

    admin commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import {Layout} from './layout/layout/layout';
 import {Dashboard} from './components/dashboard/dashboard';
-import {Analyst} from './components/analyst/analyst';
+
 import { Inventory } from './components/inventory/inventory';
 import { Customer } from './components/customer/customer';
 import { Transaction } from './components/transaction/transaction';
-import { Orders } from './components/orders/orders';
+
 import { LoginSignup } from './components/login-signup/login-signup';
 import {authGuard} from './security/guard/auth-guard';
 import {Location} from './components/location/location'
@@ -25,9 +25,6 @@ export const routes: Routes = [
             path:'dashboard',
             component: Dashboard
             },{
-                path: 'analyst',
-                component: Analyst
-            },{
                 path:'inventory',
                 component: Inventory
             },{
@@ -36,9 +33,6 @@ export const routes: Routes = [
             },{
                 path:'transactions',
                 component: Transaction
-            },{
-                path:'orders',
-                component: Orders
             },{
                 path:'location',
                 component:Location
